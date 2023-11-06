0334ad8cddbdcf0448eca4aca4d66d50f31a7be5
giancarlosds <giancarlosds@gmail.com>
2023-11-06 11:47:56 -0500

    Bucles

---
+++ _CarpetaHenry_/bucles.js
@@ -0,0 +1,13 @@
+var suma = 0;
+
+//Ciclo FOR: Se usa cuando sabemos la cantidad de iteraciones que queremos ejecutar
+/* for(i = 0; i < 10; i++){
+    suma++;
+    console.log(suma);
+} */
+
+//WHILE: cuando no se tiene la certeza de las iteraciones a usar hasta finalizar la ejecución
+while (suma < 10) {
+    suma ++;
+    console.log(suma);
+}
