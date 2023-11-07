c52b5e3fcbd9320a26a50bd419b971c0140fb134
giancarlosds <giancarlosds@gmail.com>
2023-11-07 09:59:30 -0500

    Arrays

---
+++ _CarpetaHenry_/arrays.js
@@ -0,0 +1,7 @@
+var listaDeCompras = [];
+listaDeCompras[3] = 'Tomates';
+listaDeCompras[1] = 'Lechuga';
+
+console.log(listaDeCompras.length);
+/* var elementoDelArray = listaDeCompras[1];
+console.log(elementoDelArray); */
